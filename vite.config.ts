d5fcca65dd3a2a7dbdf3ee06b1b6ace5a371603b
prawinrajulu <praveenrajulu9@gmail.com>
2026-04-15 15:21:00 +0530

    error

--- vite.config.ts
+++ vite.config.ts
@@ -19,4 +19,12 @@ export default defineConfig({
 
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:10000',
+        changeOrigin: true,
+      }
+    }
+  }
 })
